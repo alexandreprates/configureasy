c093089cbbfae74fa354b0111ca3307dd10970f3
Alexandre Prates <ajfprates@gmail.com>
2015-03-07 11:41:25 -0300

    Config is now load sub hash as objects

--- lib/configureasy/config.rb
+++ lib/configureasy/config.rb
@@ -7,9 +7,35 @@ require 'ostruct'
 #   >> Confugureasy::Config.new(foo: 'foo').foo
 #   => 'foo'
 #
+#  >> Configureasy::Config.new(:foo => {:bar => :value}).foo.bar
+#  => :value
+#
+# You can access values using [] too
+#
+# Example
+#    >> Configureasy::Config.new(foo: 'bar')[:foo]
+#    => 'bar'
+#
+# Beside you retrive data as hash
+#
+# Example
+#    >> Configureasy::Config(foo: 'bar').as_hash
+#    => {:foo => 'bar'}
 class Configureasy::Config < OpenStruct
+
+  # Convert a hash data into methods recursively
+  def initialize(params = {})
+    @hash = params
+    params = params.inject({}) do |hash, data|
+      key, value = data
+      value = self.class.new(value) if value.is_a? Hash
+      hash.merge key.to_sym => value
+    end
+    super params
+  end
+
   # return config as hash
-  def raw_content
-    @table
+  def as_hash
+    @hash
   end
 end
--- spec/lib/configureasy/config_spec.rb
+++ spec/lib/configureasy/config_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe Configureasy::Config do
-  describe ".new" do
-    it "should store key/value as methods" do
-      expect(Configureasy::Config.new(foo: 'bar').foo).to eq('bar')
+  let(:values) { {'first_level' => {'second_level' => 'second_level_value'}, 'single_level' => 'single_level_value'} }
+  let(:config) { described_class.new(values) }
+  describe '.new' do
+    it 'convert multi level hash' do
+      expect(config.first_level.second_level).to eq 'second_level_value'
     end
   end
 
-  describe "#as_hash" do
-
+  describe '#[]' do
+    it 'return value for a key' do
+      expect(config[:single_level]).to eq 'single_level_value'
+    end
   end
+
+  describe '#.as_hash' do
+    it "return raw content" do
+      expect(config.as_hash).to eq values
+    end
+  end
+
 end
